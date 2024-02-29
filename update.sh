4365358eb997612fa16c9c8704f6f56b8ae496e0
dnsmpi <159456276+dnsmpi@users.noreply.github.com>
2024-02-29 15:08:09 +0800

    use one single regex

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
-# Update publish/geosite.dat
-./domain-list-custom -excludeattrs "" -exportlists "" -togfwlist ""
+# Read the domain list into regex format
+domains=$(sed 's/\./\\./g' data/allow | paste -sd '|' -)
+
+# Build the one single regex geosite file
+mkdir -p build
+echo "regexp:(^|\\.)(${domains})\$" > build/allow
+
+# Generate publish/geosite.dat
+./domain-list-custom -datapath "build" -excludeattrs "" -exportlists "" -togfwlist ""
 
 # Hot reload OpenGFW by sending SIGHUP signal
 if pgrep OpenGFW >/dev/null; then
